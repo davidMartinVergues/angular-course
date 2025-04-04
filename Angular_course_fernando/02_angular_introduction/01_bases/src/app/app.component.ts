dde2aca040167428d2846c1cb00daa66b6e2617b
David Martin Vergues <david@easomagno.com>
2025-04-04 14:05:06 +0200

    u

--- Angular_course_fernando/02_angular_introduction/01_bases/src/app/app.component.ts
+++ Angular_course_fernando/02_angular_introduction/01_bases/src/app/app.component.ts
@@ -48,7 +48,7 @@ export class AppComponent {
       video: {
         width: { ideal: 640 },
         height: { ideal: 480 },
-        facingMode: this.useFrontCamera ? 'user' : 'environment',
+        facingMode: {exact: this.useFrontCamera ? 'user' : 'environment'},
       },
     };
 
