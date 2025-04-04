99df1d0dfa9c04e74ff501c1c5272452f323af6d
David Martin Vergues <david@easomagno.com>
2025-04-04 13:33:19 +0200

    up

--- Angular_course_fernando/02_angular_introduction/01_bases/src/app/app.component.ts
+++ Angular_course_fernando/02_angular_introduction/01_bases/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +24,86 @@ export class AppComponent {
     this.counter = 10
   }
 
+  @ViewChild('video') videoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+
+  public isLoading: boolean = true;
+  public capturedImage: string = '';
+  public useFrontCamera = false;
+
+  private stream: MediaStream | null = null;
+
+  async ngAfterViewInit() {
+    await this.initCamera();
+  }
+
+  ngOnDestroy(): void {
+    //this.stopCamera();
+  }
+
+  async initCamera() {
+    this.isLoading = true;
+
+    const constraints: MediaStreamConstraints = {
+      video: {
+        width: { ideal: 640 },
+        height: { ideal: 480 },
+       // facingMode: this.useFrontCamera ? 'user' : 'environment',
+      },
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      console.log('llegan datos');
+
+      this.stream = stream;
+      this.videoRef.nativeElement.srcObject = stream;
+      this.videoRef.nativeElement.play();
+    })
+    .catch((err) => {
+      console.error('Error al acceder a la cámara:', err);
+      this.isLoading = false;
+    })
+
+    // try {
+
+
+    //   // this.stream = await navigator.mediaDevices.getUserMedia(constraints);
+    //   // this.videoRef.nativeElement.srcObject = this.stream;
+    //   // this.isLoading = false;
+    // } catch (err) {
+    //   console.error('No se pudo acceder a la cámara:', err);
+    //   this.isLoading = false;
+    // }
+  }
+
+  captureImage() {
+    const video = this.videoRef.nativeElement;
+    const canvas = this.canvasRef.nativeElement;
+    const context = canvas.getContext('2d');
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context?.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    this.capturedImage = canvas.toDataURL('image/png');
+  }
+
+  toggleCamera() {
+    this.useFrontCamera = !this.useFrontCamera;
+    this.stopCamera();
+    this.initCamera();
+  }
+
+  stopCamera() {
+    // if (this.stream) {
+    //   this.stream.getTracks().forEach((track) => track.stop());
+    //   this.stream = null;
+    // }
+  }
+
 
 
 }
+
+
